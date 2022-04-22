3bc02c4f523ccf03f911be21635adc1c3317c8a2
Volodymyr Kovaliv <volodymyr.kovaliv@pm.bet>
2022-04-22 16:43:16 +0300

    added create feature

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 import { setDefaultContent } from "../../redux/actions";
-
+import store from "../../store/index";
 import { Wrapper, StyledButton, DefaultButton } from "./Header.style";
 
 const Header = () => {
@@ -12,18 +12,29 @@ const Header = () => {
 
   const handleSetDefaultCOntent = () => {
     dispatch(setDefaultContent());
-    history.push('/');
+    history.push("/");
+  };
+
+  const handleCreateSite = () => {
+    const state = store.getState();
+
+    fetch('http://localhost:6069/api/wsite/data/set', {
+      method: "POST",
+      body: JSON.stringify(state),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   };
 
   return (
     <Wrapper>
-      <DefaultButton
-        type="primary"
-        onClick={handleSetDefaultCOntent}
-      >
+      <DefaultButton type="primary" onClick={handleSetDefaultCOntent}>
         Заповнити дефортним контентом
       </DefaultButton>
-      <StyledButton type="primary">Створити</StyledButton>
+      <StyledButton type="primary" onClick={handleCreateSite}>
+        Створити
+      </StyledButton>
     </Wrapper>
   );
 };
